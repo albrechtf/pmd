3d30226aef1493233d5a00d239f9b54f2a280a67
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-09-03 15:11:11 +0200

    Include diffs in repository for easier build

--- pmd-vb/createPatch.sh
+++ pmd-vb/createPatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-diff -c target/generated-sources/com/github/albrechtf/pmd/lang/vb/ast/$1.java src/gen/java-customized-ast/$1.java > src/gen/java-fragments/ast/$1.patch
+diff -c target/generated-sources/com/github/albrechtf/pmd/lang/vb/ast/$1.java src/gen/java-customized-ast/$1.java > src/gen/java-fragments/ast/$1.diff
